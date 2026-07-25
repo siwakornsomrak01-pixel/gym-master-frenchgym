26a14f433748f7c30fe204659e73671e16ed422a
Toppu30 <siwakorn.so.64@ubu.ac.th>
2026-07-25 15:40:48 +0700

    feat: configure Firebase for frenchgym-database

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // คัดลอกค่า Firebase Configuration จากหน้าจอ Firebase Console มาวางที่นี่ครับ
 // โดยใน Firebase Console ไปที่: Project Settings -> ด้านล่างสุดหัวข้อ "Your apps"
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBX2Sfdjy6icE_U-7dw7v-z8cLhJcnsV4E",
+  authDomain: "frenchgym-database.firebaseapp.com",
+  projectId: "frenchgym-database",
+  storageBucket: "frenchgym-database.firebasestorage.app",
+  messagingSenderId: "863529314448",
+  appId: "1:863529314448:web:51d4b6732bd59090041015"
 };
